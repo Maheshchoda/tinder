a5a649e343086b03195ddb82bd90a860496199b7
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-20 12:20:49 +0530

    Added Login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center align-middle vertical-center">
+      <LoginForm />
+    </div>
+  );
 }
